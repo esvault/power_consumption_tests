7886a4942732db0128b8305c11c61e18dfaf5ea3
Ivan Azarnikov <54893829+esvault@users.noreply.github.com>
2024-03-30 18:28:51 +0300

    added skip welcome activity in MxPlayerTest

--- mxplayer_test.py
+++ mxplayer_test.py
@@ -5,7 +5,7 @@ from time import sleep
 class MxPlayerTest(BaseTest):
     def __init__(self, adb_path):
         super(MxPlayerTest, self).__init__(adb_path)
-        self.adb = adb_path
+        self.name = 'video_test'
         self.package_name = 'com.mxtech.videoplayer.ad'
         self.activity = 'com.mxtech.videoplayer.ad.ActivityWelcomeMX'
 
@@ -14,18 +14,28 @@ class MxPlayerTest(BaseTest):
                     f'{self.package_name}/{self.activity}'.split(' ')
         
         _ = sp.check_output(start_app)
+        sleep(2)
 
-        
+        self._skip_welcome_activity()
+        sleep(1)
+
         select_folder = f'{self.adb} shell input tap {int(self.x_max / 2)} {int(self.y_max / 4.3)}'.split(' ')
         select_video = f'{self.adb} shell input tap {int(self.x_max / 2)} {int(self.y_max / 6.9)}'.split(' ')
         
-        sleep(2)
         _ = sp.check_output(select_folder)
         _ = sp.check_output(select_video)
 
         sleep(time_sec)
         self.close_recent_app()
         self._kill_app()
+
+    def _skip_welcome_activity(self):
+        select_location = f'{self.adb} shell input tap {int(self.x_max / 2)} {int(self.y_max / 3.21)}'.split(' ')
+        confirm = f'{self.adb} shell input tap {int(self.x_max / 2)} {int(self.y_max / 2)}'.split(' ')
+
+        _ = sp.check_output(select_location)
+        sleep(0.2)
+        _ = sp.check_output(confirm)
 
 
 if __name__ == "__main__":
